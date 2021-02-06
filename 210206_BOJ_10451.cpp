ca2e8eecf091a7eddf2cb22b93130fadb5298a4f
JH <lyn00227@naver.com>
2021-02-06 11:20:30 +0900

    백준 순열 사이클

---
+++ 210206_BOJ_10451.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int T = 0;
+int N = 0;
+//vector<int> v;
+//vector<bool> visit;
+int v[1001];
+bool visit[1001];
+int cnt = 0;
+int pre = 0;
+
+void findAnswer(int s) {
+
+	visit[s] = true;
+
+	if (pre == v[s]) {
+		cnt++;
+		return;
+	}
+	else {
+		findAnswer(v[s]);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL), cout.tie(NULL);
+
+	cin >> T;
+
+	while (T--) {
+		cin >> N;
+		for (int i = 1; i <= N; ++i) {
+			cin >> v[i];
+		}
+
+		for (int i = 1; i <= N; ++i)
+			visit[i] = false;
+
+		for (int i = 1; i <= N; ++i) {
+			if (!visit[i]) {
+				pre = i;
+				findAnswer(i);
+			}
+		}
+
+		cout << cnt << "\n";
+		cnt = 0;
+	}
+
+	return 0;
+}
