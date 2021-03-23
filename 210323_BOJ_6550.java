4b1c290c6f55eb63419fd64c10c30a215ecc78d9
JH <lyn00227@naver.com>
2021-03-23 18:39:27 +0900

    백준 부분 문자열, 문자열 EOF 문제 해결하지 못함

---
+++ 210323_BOJ_6550.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String st = br.readLine();
+
+            if (st.equals("\n"))
+                break;
+
+            String[] arr = st.split(" ");
+            String s = arr[0];
+            String t = arr[1];
+
+            int a = 0, b = 0;
+            for(; b < t.length() && a < s.length(); ++b) {
+                if (s.charAt(a) == t.charAt(b))
+                    a++;
+            }
+
+            if (a == s.length())
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+        }
+    }
+}
