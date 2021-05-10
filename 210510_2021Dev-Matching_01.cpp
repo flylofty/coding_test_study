7b03253c3c1823835e850b45c7daa3ea3944368d
JH <lyn00227@naver.com>
2021-05-10 16:07:44 +0900

    프로그래머스 로또의 최고 순위와 최저 순위

--- 210510_2021Dev-Matching_01.cpp
+++ 210510_2021Dev-Matching_01.cpp
@@ -2,26 +2,19 @@
 
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-
-bool winLottos[46]{ false };
 
 int findRank(int same, int unKnown) {
 
 	int cnt = same + unKnown;
-	if (cnt == 6) return 1;
-	else if (cnt == 5) return 2;
-	else if (cnt == 4) return 3;
-	else if (cnt == 3) return 4;
-	else if (cnt == 2) return 5;
-	else return 6;
+	return (cnt > 1) ? (7 - cnt) : 6;
 }
 
 vector<int> solution(vector<int> lottos, vector<int> win_nums) {
 	vector<int> answer(2);
 
+	bool winLottos[46]{ false };
 	for (int i = 0; i < win_nums.size(); ++i)
 		winLottos[win_nums[i]] = true;
 
@@ -47,9 +40,6 @@ vector<int> solution(vector<int> lottos, vector<int> win_nums) {
 }
 
 int main() {
-
-	// 0 0 1(O) 25 31(O) 44 == > 2개 ~ 4개
-	// 1 6 10 19 31 45
 
 	//vector<int> lottos{ 44, 1, 0, 0, 31, 25 };
 	//vector<int> win_nums{ 31, 10, 45, 1, 6, 19 };
