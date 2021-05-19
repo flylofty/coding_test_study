ca27781fb7765efeacb4dd507a1e15e950f9e0c2
JH <lyn00227@naver.com>
2021-05-19 13:23:22 +0900

    백준 투자의 귀재 배주형, 브루트포스

--- 210519_BOJ_19947.cpp
+++ 210519_BOJ_19947.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int H, Y, Max = 0;
 double rate[6]{ 0, 1.05, 0, 1.2, 0, 1.35 };
 int year[3]{ 1, 3, 5 };
-vector<int> v;
-vector<int> allCase;
 
-void findCase(int sum) {
+void findCase(int sum, int total) {
 
 	if (sum == Y) {
-
-		int total = H;
-		for (int i = 0; i < v.size(); ++i) {
-
-			total = (int)(total * rate[v[i]]);
-		}
 
 		if (total > Max)
 			Max = total;
@@ -26,11 +17,8 @@ void findCase(int sum) {
 	}
 
 	for (int i = 0; i < 3; ++i) {
-		if (sum + year[i] <= Y) {
-			v.push_back(year[i]);
-			findCase(sum + year[i]);
-			v.pop_back();
-		}
+		if (sum + year[i] <= Y)
+			findCase(sum + year[i], total * rate[year[i]]);
 	}
 }
 
@@ -38,7 +26,7 @@ int main() {
 
 	cin >> H >> Y;
 
-	findCase(0);
+	findCase(0, H);
 
 	cout << Max;
 
