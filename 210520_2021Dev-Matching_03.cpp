54f8fd5029ee93ef4c158128d3616c4db19f2883
JH <lyn00227@naver.com>
2021-05-20 16:45:00 +0900

    프로그래머스 다단계 칫솔 판매

---
+++ 210520_2021Dev-Matching_03.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
+	vector<int> answer;
+
+	map<string, pair<int, int>> position;
+	//answer.push_back(0);
+	//position["-"] = { answer.size() - 1, -1 };
+	for (int i = 0; i < enroll.size(); ++i) {
+		answer.push_back(0);
+		position[enroll[i]] = { i, i };
+	}
+
+	for (int i = 0; i < seller.size(); ++i) {
+
+		string child = seller[i];
+		int profit = amount[i] * 100;
+
+		while (child != "-") {
+
+			int a = 0;
+			int b = profit * 0.1;
+
+			if (b < 1) {
+				a = profit;
+				b = 0;
+			}
+			else {
+				a = profit - b;
+			}
+
+			answer[position[child].first] += a;
+
+			string parent = referral[position[child].second];
+
+			child = parent;
+			profit = b;
+		}
+	}
+
+	return answer;
+}
+
+int main() {
+
+	/*vector<string> enroll{ "john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young" };
+	vector<string> referral{ "-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward" };
+	vector<string> seller{ "young", "john", "tod", "emily", "mary" };
+	vector<int> amount{ 12, 4, 2, 5, 10 };
+	vector<int> res = solution(enroll, referral, seller, amount);*/
+
+	vector<string> enroll{ "john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young" };
+	vector<string> referral{ "-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward" };
+	vector<string> seller{ "sam", "emily", "jaimie", "edward" };
+	vector<int> amount{ 2, 3, 5, 4 };
+	vector<int> res = solution(enroll, referral, seller, amount);
+
+
+	for (int i = 0; i < res.size(); ++i)
+		cout << res[i] << " ";
+	cout << "\n";
+
+	return 0;
+}
